a210263889dbd423262202a78fe78142cad84418
Justin Carville <jccarville@gmail.com>
2023-09-28 11:57:40 +0900

    Part 10: Create, Update, Delete posts: added views for each, buttons for each post, authentication checks for logged in user

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
-
-# dummy data
-# list of dictionaries, each dict has post content
-# posts = [
-#     {
-#         'author': 'Mujo',
-#         'title': 'First blog',
-#         'content': 'Read my post here',
-#         'date_posted': 'September 28, 2023'
-#     },
-#     {
-#         'author': 'John Doe',
-#         'title': 'John\'s story',
-#         'content': 'Here\'s my post',
-#         'date_posted': 'September 28, 2023'
-#     }
-# ]
 
 # return what we want user to see from "home" page
 def home(request):
@@ -26,6 +11,49 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 # return what we want user to see from "about" page
 def about(request):
